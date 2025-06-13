5f709b682ae869dbf0b6f93dd091211580b8822f
yaniguan <168690533+yaniguan@users.noreply.github.com>
2025-06-13 02:12:13 +0000

    Deploying to gh-pages from @ yaniguan/yaniguan.github.io@e55194c91ef9607cf3e9b1fdd4a4732af36fdde2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,8 +37,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv-jan-31-2025-last-update",
-          title: "cv (Jan 31, 2025 (last update))",
+        },{id: "nav-cv",
+          title: "CV",
           description: "Please check my CV here, updated Jan 31 2025",
           section: "Navigation",
           handler: () => {
@@ -474,14 +474,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%79%61%6E%69%67%75%61%6E@%75%63%6C%61.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/yaniguan", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/yaniguan", "_blank");
         },
       },{
         id: 'social-rss',
@@ -495,14 +502,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=eb_DB84AAAAJ&hl=en", "_blank");
         },
       },{
       id: 'light-theme',
